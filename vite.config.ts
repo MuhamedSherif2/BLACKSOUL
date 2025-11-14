1b5601237cb406f326164f72d3c441e77f42951c
mohammed sherif <muhamedsherif2612@gmail.com>
2025-11-14 22:27:14 +0200

    ffffffff

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: '/index.html',
-    },
-  },
+  plugins: [
+    tailwindcss(),
+  ],
 })
